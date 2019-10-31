c07facf4f5a133ca53aa92a256ba1a50c2343450
mxwllndrsn <mxwllndrsn@gmail.com>
2019-10-31 09:55:26 -0700

    intial results rendering success

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
+// index.js
+// xml-p
+// initial test
+
 const http = require('http');
+const path = require('path');
 const express = require('express');
 const app = express();
 
-// serve settings
+const Parser = require('rss-parser');
+const parser = new Parser();
 
+
+// serve settings
 http.createServer(app).listen(8080);
 app.listen(8000, function () {
 	console.log('listening port: 8080');
 });
 
-let Parser = require('rss-parser');
-let parser = new Parser();
 
+// static & views
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+// callbacks
 app.get('/', function(req, res){
 	console.log('GET \'/\'');
 	parser.parseURL('http://aljazeera.com/xml/rss/all.xml', function(err, feed){
 		if(err) throw err;
 		console.log('Success: ', feed.title);
+		res.render('index', {
+			title: feed.title,
+			link: feed.link
+		});
 	});
+
 });
